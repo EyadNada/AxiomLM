ff0a9e84d2c5df30e3c62997ef7d23d73ae7b328
EyadNada <cosign2006@gmail.com>
2026-08-18 05:35:24 +0300

    chore: reset notebook cell execution counts and update local development settings

--- brain/train_gpt2.py
+++ brain/train_gpt2.py
@@ -121,6 +121,17 @@ class GPT(nn.Module):
 
         #weight sharing scheme
         cast(nn.Embedding, self.transformer['wte']).weight = self.lm_head.weight
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
         
 
     def forward(self, idx, targets=None):
@@ -302,3 +313,5 @@ while x.size(1) < max_length:
 #    tokens = x[i, :max_length].tolist()
 #    decoded = enc.decode(tokens)
 #    print(">", decoded)
+
+#01:20:13
