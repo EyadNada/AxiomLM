99b4b3f5bd518e1ca16963362ca15b0b2fea5b4b
EyadNada <cosign2006@gmail.com>
2026-08-20 16:36:06 +0300

    feat: update training log output format to include additional loss metric and adjust time precision

--- brain/train_gpt2.py
+++ brain/train_gpt2.py
@@ -306,7 +306,7 @@ for i in range(50):
     t1 = time.time()
     dt = (t1 - t0) * 1000 # time difference in milliseconds
     tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
-    print(f"step {i:2d}, loss: {loss.item():.6f}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
+    print(f"step {i:2d} | loss: {loss.item():.6f} | loss: {loss().item:.6f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
