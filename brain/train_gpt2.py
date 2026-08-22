b5d82a07b9c5bb45b916b6e1e11ea71a33d96bd4
EyadNada <cosign2006@gmail.com>
2026-08-22 20:15:09 +0300

    fix: update loss logging to reference loss.accum instead of loss item

--- brain/train_gpt2.py
+++ brain/train_gpt2.py
@@ -370,7 +370,7 @@ for step in range(max_steps):
     dt = t1 - t0 # time difference in seconds
     tokens_processed = train_loader.B * train_loader.T * grad_accum_steps
     tokens_per_sec = tokens_processed / dt
-    print(f"step {step:4d} | loss: {loss.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
+    print(f"step {step:4d} | loss: {loss.accum.item():.6f} | lr: {lr:.4e} | norm: {norm:.4f} | dt: {dt*1000:.2f}ms | tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
